d62423947f2c5031722300450001beace0725a83
Oleg Goncharov <oleg.gnch@gmail.com>
2021-07-26 01:07:16 +0300

    Implemented opening and closing config file in loop

--- cmd/ozon-tip-api/main.go
+++ cmd/ozon-tip-api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("This is Ozon Tip API")
+
+	openFile := func(path string) error {
+		file, err := os.Open(path)
+		defer file.Close()
+		return err
+	}
+	for i := 0; i < 10; i++ {
+		openFile("config.txt")
+	}
 }
